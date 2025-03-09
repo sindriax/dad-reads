fadade2716d22a1f2fe4b7bc0eaa05b413badc94
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-09 18:51:48 +0100

    add footer component and update layout; include Google Books domain for images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Dad Reads",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
